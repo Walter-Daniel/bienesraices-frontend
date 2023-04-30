3c424ee5ee7ee9929397d58918748aee66d49e60
Walter <walterdcarrizo.19@gmail.com>
2023-04-29 23:39:12 -0300

    Se agrego el responsive al navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { SmileOutlined, DownOutlined } from '@ant-design/icons';
-
-import { Alert, Dropdown, Layout, Space } from 'antd';
+import { Layout } from 'antd';
 const { Header, Footer, Content } = Layout;
 
 import React from 'react'
