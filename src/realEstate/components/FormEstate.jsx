f3581b79f307a982900bdb18e6e7d5cf2ccad0a1
Walter <walterdcarrizo.19@gmail.com>
2023-05-01 17:32:17 -0300

    Modal del formulario

--- src/realEstate/components/FormEstate.jsx
+++ src/realEstate/components/FormEstate.jsx
@@ -1,11 +1,98 @@
-import React from 'react'
+import { DatePicker, Form, Input, InputNumber, Radio, Select } from 'antd';
 
-export const FormEstate = () => {
+export const FormEstate = ({form}) => {
+  
   return (
-    <div>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-    </div>
+    <Form
+        form={form}
+        layout="vertical"
+        name="form_in_modal"
+        initialValues={{
+          modifier: 'public',
+        }}
+      >
+        <Form.Item
+          name="title"
+          label="Título"
+          rules={[
+            {
+              required: true,
+              message: 'Por favor, ingrese el título del anuncio.'
+            },
+          ]}
+        >
+          <Input placeholder='3 Amb Todo Externo con Balcón - ¡Oportunidad!'/>
+        </Form.Item>
+        <Form.Item
+          name="category"
+          label="Categoría"
+          rules={[
+            {
+              required: true,
+              message: 'Por favor, seleccione una categoría.'
+            },
+          ]}
+        >
+          <Select placeholder='--- Seleccione una categoría ---'>
+            <Select.Option value="demo">Demo</Select.Option>
+          </Select>
+        </Form.Item>
+        <Form.Item
+          name='price'
+          label= 'Precio'
+          rules={[{ 
+              required: true, 
+              message: "Por favor, ingrese el precio de la propiedad",
+              pattern: new RegExp(/^[0-9]+$/)
+          }]}
+        >
+          <Input
+              placeholder='$9999'
+              maxLength={12}
+              addonBefore={
+                  <Form.Item
+                    name='currency'
+                    noStyle
+                    style={{
+                      width: 70,
+                    }}
+                    rules={[
+                      {
+                        required: true,
+                        message: 'Por favor, seleccione un tipo de moneda'
+                      }
+                    ]}
+                  >
+                    <Select placeholder='--- Seleccione una moneda ---'>
+                      <Select.Option value='peso'>Pesos - $</Select.Option>
+                      <Select.Option value="dollar">USD - $</Select.Option>
+                      <Select.Option value="euro">EUR - €</Select.Option>
+                    </Select>
+                  </Form.Item>
+              }
+              style={{
+                width: '100%',
+              }}
+          />
+        </Form.Item>
+        <Form.Item 
+          name="description" 
+          label="Description"
+          rules={[
+            {
+              required: true,
+              message: 'Por favor, ingrese información sobre la propiedad'
+            }
+          ]}>
+          <Input type="textarea" />
+        </Form.Item>
+        
+        <Form.Item name="modifier" className="collection-create-form_last-form-item">
+          <Radio.Group>
+            <Radio value="public">Público</Radio>
+            <Radio value="private">Privado</Radio>
+          </Radio.Group>
+        </Form.Item>
+      </Form>
   )
 }
--- src/realEstate/components/ModalEstate.jsx
+++ src/realEstate/components/ModalEstate.jsx
@@ -1,20 +1,41 @@
+import { useForm } from "antd/es/form/Form";
 import { FormEstate } from "./FormEstate";
 import { Modal } from "antd";
 
 
 export const ModalEstate = ({ isModalOpen, setIsModalOpen }) => {
   
-    const handleOk = () => {
+    // const handleOk = () => {
+    //   setIsModalOpen(false);
+    // };
+    const handleCancel = () => {
       setIsModalOpen(false);
     };
-    const handleCancel = () => {
+
+    const [form] = useForm();
+
+    const onCreate = (values) => {
+      console.log('Received values of form: ', values);
       setIsModalOpen(false);
     };
 
   return (
     <>
-        <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-            <FormEstate />
+        <Modal title="Propiedad en venta" 
+               open={isModalOpen} 
+               onCancel={handleCancel}
+               onOk={() => {
+                form
+                .validateFields()
+                .then((values) => {
+                  form.resetFields();
+                  onCreate(values);
+                })
+                .catch((info) => {
+                  console.log('Validate Failed:', info);
+                });
+              }}>
+            <FormEstate form={form}/>
         </Modal>
     </>
   )
